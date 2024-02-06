94cd9ee328761bdc2e8c8048ac456f8ea82617b8
imam76 <asepimam335@gmail.com>
2024-02-06 18:19:46 +0700

    Next.js lates version v.0.1.1

--- src/app/greet.js
+++ src/app/greet.js
@@ -7,7 +7,7 @@ export default function Greet() {
   const [greeting, setGreeting] = useState('');
 
   useEffect(() => {
-    invoke('greet', { name: 'Next.js lates v.0.1.1' })
+    invoke('greet', { name: 'Next.js lates version v.0.1.1' })
       .then(result => setGreeting(result))
       .catch(console.error)
   }, [])
